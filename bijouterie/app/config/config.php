5afe27083dbd9c2c06ce51d1b299b0555316a8d3
kandirou0 <simo.kandirou15@gmail.com>
2022-12-07 20:05:20 +0100

    update

--- bijouterie/app/config/config.php
+++ bijouterie/app/config/config.php
@@ -2,6 +2,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/brief5');
+  define('URLROOT', 'http://localhost/bijouterie_MVC/bijouterie');
   // Site Name
   define('SITENAME', 'Bijouterie');
--- bijouterie/app/models/User.php
+++ bijouterie/app/models/User.php
@@ -4,14 +4,14 @@ class User extends database{
     function __construct() {}
 
     public function login($email,$pass){
-        $sql = "SELECT `id_u`, `name`, `email`, `pass` FROM `user` WHERE email=:email and pass=:mdp";
+        $sql = "SELECT `id_a`, `name`, `email`, `pass` FROM `admin` WHERE email=:email and pass=:mdp";
         $stmt=$this->openConnection()->prepare($sql);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':mdp', $pass);
         $stmt->execute();
         if($stmt->rowCount()==1){
             $res=$stmt->fetch(PDO::FETCH_ASSOC);
-            $_SESSION['id']=$res['id_u'];
+            $_SESSION['id']=$res['id_a'];
             $_SESSION['name']=$res['name'];
             $_SESSION['email']=$res['email'];
             $_SESSION['password']=$res['password'];
